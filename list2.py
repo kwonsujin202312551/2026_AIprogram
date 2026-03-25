2259e0f552b6ce210e0e7dd61caef451ee9da2fd
kwonsujin202312551 <ksujin11250@gmail.com>
2026-03-25 16:47:26 +0900

    2026.3.25

---
+++ list2.py
@@ -0,0 +1,37 @@
+score = [87, 84, 95, 67, 88, 94, 63]
+print(score)
+print(type(score))
+
+print('---')
+
+score = [87, 84, 95, 67, 88, 94, 63]
+for i in score:
+    print(i)
+
+print('---')
+
+score = [87, 84, 95, 67, 88, 94, 63]
+names = ["영호", "순자", "영식", "순희"]
+for i in names:
+    print(i)
+
+print('---')
+
+score = [87, 84, 95, 67, 88, 94, 63]
+names = ["영호", "순자", "영식", "순희", score]
+for i in names:
+    print(i)
+
+print('---')
+
+
+score = [87, 84, 95, 67, 88, 94, 63]
+names = ["영호", "순자", "영식", "순희", score]
+addressbook = ["영호", 25, "010-1234-5678"], ["순자", 30, "010-1234-9678"]
+for i in addressbook:
+    print(i)
+
+print('---')
+
+ri = list(range(5))
+print(ri)
