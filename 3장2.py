de8c641115755e64ebfa9c1d3cea21c6a5d235ff
kwonsujin202312551 <ksujin11250@gmail.com>
2026-03-25 16:22:54 +0900

    2026.3.25

---
+++ 3장2.py
@@ -0,0 +1,11 @@
+for i in range(5):
+    print(i)
+
+for i in range(2,7):
+    print(i)
+
+for i in range(5):
+    print('welcome to everyone')
+#밑줄
+for _ in range(5):
+    print('welcome te everyone')
