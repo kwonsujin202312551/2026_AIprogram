999b51f34836ef24265ba7a36a131412719100f2
kwonsujin202312551 <ksujin11250@gmail.com>
2026-03-27 10:16:32 +0900

    2026-3-27

---
+++ 3coding.py
@@ -0,0 +1,21 @@
+
+#for in range 표현식
+for i in range(5):
+    print(i, end = '')
+
+for i in range(5):
+    print(i, '')
+
+for i in range(0, 5):
+    print(i, end= '')
+
+for i in range(5):
+    print(i)
+    print(i, end = '\n')
+
+for i in range(0, 10, 2):
+    print(i, end = '')
+print()
+
+for i in range(-2, -10, -2):
+    print(i, end ='')
