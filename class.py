19ecaed5431d041d2342337f1e7cb95c1f4698dd
kwonsujin202312551 <ksujin11250@gmail.com>
2026-04-17 10:03:56 +0900

    2026.4.17

---
+++ class.py
@@ -0,0 +1,15 @@
+class Cat:
+    def __init__(self, name, color = '흰색'):
+        self.name = name
+        self.color = color
+
+    def meow(self):
+        print('내 이름은 {}, 색깔은 {}, 야옹 야옹'.format(self.name, self.color))
+
+nabi = Cat('나비', '검정색')
+nero = Cat('네로', '흰색')
+mimi = Cat('미미', '갈색')
+
+nabi.meow()
+nero.meow()
+mimi.meow()
